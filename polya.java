da3e791e396dd037fcadb997478abc42a06cfe02
Jer <zjyjer@gmail.com>
2014-07-23 22:02:42 +0800

    polya

---
+++ polya.java
@@ -0,0 +1,46 @@
+import java.math.*;
+import java.util.*;
+
+public class Main
+{
+	static Scanner cin = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		int n,t;
+		BigInteger c,sum,x,two=BigInteger.valueOf(2),four,eight;
+		four=BigInteger.valueOf(4);
+		eight=BigInteger.valueOf(8);
+		while(cin.hasNext())
+		{
+			n=cin.nextInt();
+			c=cin.nextBigInteger();
+			sum=BigInteger.ZERO;
+			if(n%2==0)
+			{
+				x=c.pow(n*n);
+				sum=sum.add(x);
+				x=two.multiply(c.pow(n*n/4));
+				sum=sum.add(x);
+				x=c.pow(n*n/2);
+				sum=sum.add(x);
+				x=two.multiply(c.pow(n*n/2));
+				sum=sum.add(x);
+				x=two.multiply(c.pow(n+(n*n-n)/2));
+				sum=sum.add(x);
+			}
+			else
+			{
+				x=c.pow(n*n);
+				sum=sum.add(x);
+				x=two.multiply(c.pow((n*n-1)/4+1));
+				sum=sum.add(x);
+				x=c.pow((n*n+1)/2);
+				sum=sum.add(x);
+				x=four.multiply(c.pow((n*n+n)/2));
+				sum=sum.add(x);
+			}
+			sum=sum.divide(eight);
+			System.out.printfln(sum);
+		}
+	}
+}
