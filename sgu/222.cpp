a5a018921968c7c289c0ab82958f3f35f23fa3bd
Jer <zjyjer@gmail.com>
2014-07-31 09:50:41 +0800

    sgu 222

---
+++ sgu/222.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+int f[11];
+
+int c(int n,int k)
+{
+	int ret=1;
+	for(int i=1;i<=k;i++)
+		ret=ret*(n-i+1)/i;
+	return ret;
+}
+int main()
+{
+	f[0]=1;
+	for(int i=1;i<=10;i++)
+		f[i]=i*f[i-1];
+	int n,k;
+	scanf("%d %d",&n,&k);
+	if(k>n || k==0)
+	{
+		printf("0");
+		return 0;
+	}
+	else printf("%d\n",f[k]*c(n,k)*c(n,k));
+}
