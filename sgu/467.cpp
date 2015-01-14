e214a6d7fcdb625c4d8a7df86b3de19f94d334a2
zjyjer <zjyjer@gmail.com>
2015-01-15 00:16:33 +0800

    sgu 467

---
+++ sgu/467.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+	int cas = 0, p;
+	freopen("467.in", "r", stdin);
+	while(std::cin>>p) {
+		printf("Case #%d: ", ++cas);
+		if (p == 0)
+			std::cout << 0 << std::endl;
+		else if (p <= 78)
+			std::cout << 1 << std::endl;
+		else if(p <= 95)
+			std::cout << 2 << std::endl;
+		else if (p <= 99)
+			std::cout << 3 << std::endl;
+		else
+			std::cout << 4 << std::endl;
+	}
+	return 0;
+}
